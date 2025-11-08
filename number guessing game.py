396b113dd88d2249bf8cb54cf3dcee7534c2e811
orinion1298 <mayg.daydream@gmail.com>
2025-11-08 14:49:03 +0200

    Update number guessing game.py

--- number guessing game.py
+++ number guessing game.py
@@ -2,9 +2,11 @@ import random
 import time
 
 number = random.randint(1, 5)
-name=input("guess a number from 1 to 5: ")
-if name == number:
-  print("you won")
+guess = int(input("Guess a number from 1 to 5: "))
+
+if guess == number:
+    print("You won!")
 else:
-  print("you lose")
-print(number)
+    print("You lose.")
+
+print("The number was:", number)
