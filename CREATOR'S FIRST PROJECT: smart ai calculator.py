ac6bbcac32f1787f9071c6b8b60c00862b050a9f
orinion1298 <mayg.daydream@gmail.com>
2025-11-09 15:57:43 +0200

    Update and rename MY FIRST PROJECT: smart ai calculator.py to CREATOR'S FIRST PROJECT: smart ai calculator.py

---
+++ CREATOR'S FIRST PROJECT: smart ai calculator.py
@@ -0,0 +1,9 @@
+print("------------------------------------------------------------------------------------")
+print()
+print("hello, whats your name?")
+print()
+name=input()
+print("------------------------------------------------------------------------------------")
+print()
+print("hello," + name"
+my name is AI.PROTOTYPE_385. this is the first version, but soon i will become an smart AI calculator, (if you want you can try an version that chatGPT made, (this is what the creator want to make).")
