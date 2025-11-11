6dbe68409997e0a6c87243e24058515bc86d60a4
orinion1298 <mayg.daydream@gmail.com>
2025-11-11 16:18:31 +0200

    Update and rename python random age.py to python age scanner.py

---
+++ python age scanner.py
@@ -0,0 +1,9 @@
+number=int(input("please enter your age: "))
+if number >= 67:
+    print("you are an old man")
+elif number >= 18:
+    print("you are an adult")
+elif number <= 4:
+    print("you are a baby")
+else:
+    print("you are a child")
