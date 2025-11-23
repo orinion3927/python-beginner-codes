ff70ea148ff5b6d4c1bdd65b2fd4001c64b46749
orinion1298 <mayg.daydream@gmail.com>
2025-11-23 17:01:26 +0200

    Update CREATOR'S FIRST PROJECT: smart ai calculator.py

--- CREATOR'S FIRST PROJECT: smart ai calculator.py
+++ CREATOR'S FIRST PROJECT: smart ai calculator.py
@@ -1,10 +1,34 @@
-print("------------------------------------------------------------------------------------")
-print()
-print("hello, what's your name?")
-print()
-name = input()
-print("------------------------------------------------------------------------------------")
-print()
-print("hello, " + name)
-print("my name is AI.PROTOTYPE_385. this is the first version, but soon I will become a smart AI calculator.")
-print("(if you want, you can try a version that ChatGPT made — this is what the creator wants to make.)")
+#password = Pas$W0rd193
+import time
+
+while True:
+    a=input("please Enter password: ")
+    if a == "Pas$W0rd193":
+        print("checking...")
+        time.sleep(1)
+        print("access granted")
+        x=input("do you want to continue to the smart_AI_calculator test? (yes/no): ")
+        if x == "yes" or x == "yeah":
+            print("continuing...")
+            time.sleep(1)
+            print("------------------------------------------------------------------------------------")
+            print()
+            name = input("hello, what is your name? ")
+            print("loading...")
+            time.sleep(1)
+            print()
+            print("------------------------------------------------------------------------------------")
+            print()
+            print("hello, " + name)
+            print("my name is AI.PROTOTYPE_385. this is the first version, but soon I will become a smart AI calculator.")
+            print("(if you want, you can try a version that ChatGPT made — this is what the creator wants to make.)")
+        else:
+            print("exiting...")
+            time.sleep(1)
+            print("goodbye! see ya later!")
+    else:
+        print("access denied")
+        continue
+    break
+
+# do not copy this code or other codes from the repository "python beginner codes". (in github)
