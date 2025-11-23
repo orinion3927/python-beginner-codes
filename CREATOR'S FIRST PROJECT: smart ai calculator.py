1f10503458d9f861dd3189424a4c7443c1c703dd
orinion1298 <mayg.daydream@gmail.com>
2025-11-23 17:49:17 +0200

    Update CREATOR'S FIRST PROJECT: smart ai calculator.py

--- CREATOR'S FIRST PROJECT: smart ai calculator.py
+++ CREATOR'S FIRST PROJECT: smart ai calculator.py
@@ -1,37 +1,60 @@
 #password = Pas$W0rd193
 import time
 
-while True:
-    a=input("please Enter password: ")
-    if a == "Pas$W0rd193":
-        print("checking...")
-        time.sleep(1)
-        print("access granted")
-        x=input("do you want to continue to the smart_AI_calculator test? (yes/no): ")
-        if x == "yes" or x == "yeah":
-            time.sleep(0.1)
-            print("continuing...")
-            time.sleep(1)
+if SyntaxError == True:
+    print("there is an error in the code. please fix it and try again.")
+else:
+    while True:
+        a=input("please Enter password: ")
+        if a == "Pas$W0rd193":
             print("------------------------------------------------------------------------------------")
             print()
-            name = input("hello, what is your name? ")
-            time.sleep(0.1)
-            print("loading...")
+            print("checking...")
+            print()
             time.sleep(1)
             print()
-            print("------------------------------------------------------------------------------------")
-            print()
-            print("hello, " + name)
-            print("my name is AI.PROTOTYPE_385. this is the first version, but soon I will become a smart AI calculator.")
-            print("(if you want, you can try a version that ChatGPT made — this is what the creator wants to make.)")
+            print("access granted")
+            x=input("do you want to continue to the smart_AI_calculator test? (yes/no): ")
+            if x == "yes" or x == "yeah":
+                time.sleep(0.1)
+                print("continuing...")
+                time.sleep(1)
+                print("opening files...")
+                time.sleep(1)
+                print("starting program...")
+                time.sleep(1)
+                print("------------------------------------------------------------------------------------")
+                print()
+                name = input("hello, what is your name? ")
+                time.sleep(0.1)
+                print("loading...")
+                time.sleep(1)
+                print()
+                print("------------------------------------------------------------------------------------")
+                print()
+                print("hello, " + name)
+                print("my name is AI.PROTOTYPE_385. this is the first version, but soon I will become a smart AI calculator.")
+                print("(if you want, you can try a version that ChatGPT made — this is what the creator wants to make.)")
+                print()
+                print()
+                print()
+                print()
+                print()
+                print()
+            else:
+                time.sleep(0.1)
+                print("exiting...")
+                time.sleep(1)
+                print("goodbye! see ya later!")
+                print()
+                print()
+                print()
+                print()
+                print()
+                print()
         else:
-            time.sleep(0.1)
-            print("exiting...")
-            time.sleep(1)
-            print("goodbye! see ya later!")
-    else:
-        print("access denied")
-        continue
-    break
+            print("access denied")
+            continue
+        break
 
 # do not copy this code or other codes from the repository "python beginner codes". (in github)
