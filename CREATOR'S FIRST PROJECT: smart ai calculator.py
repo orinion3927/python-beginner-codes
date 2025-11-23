e70f5619becfaccd68e6e586c33f3ee1e7dcacf1
orinion1298 <mayg.daydream@gmail.com>
2025-11-23 17:12:56 +0200

    Update CREATOR'S FIRST PROJECT: smart ai calculator.py

--- CREATOR'S FIRST PROJECT: smart ai calculator.py
+++ CREATOR'S FIRST PROJECT: smart ai calculator.py
@@ -9,11 +9,13 @@ while True:
         print("access granted")
         x=input("do you want to continue to the smart_AI_calculator test? (yes/no): ")
         if x == "yes" or x == "yeah":
+            time.sleep(0.1)
             print("continuing...")
             time.sleep(1)
             print("------------------------------------------------------------------------------------")
             print()
             name = input("hello, what is your name? ")
+            time.sleep(0.1)
             print("loading...")
             time.sleep(1)
             print()
@@ -23,6 +25,7 @@ while True:
             print("my name is AI.PROTOTYPE_385. this is the first version, but soon I will become a smart AI calculator.")
             print("(if you want, you can try a version that ChatGPT made — this is what the creator wants to make.)")
         else:
+            time.sleep(0.1)
             print("exiting...")
             time.sleep(1)
             print("goodbye! see ya later!")
