26426951f2e322c9208b08ccb39743bb3eeed1d2
orinion1298 <mayg.daydream@gmail.com>
2025-12-03 14:07:23 +0200

    Update smart AI test.py

--- smart AI test.py
+++ smart AI test.py
@@ -3,12 +3,12 @@ import time
 
 def List():
     print("password: Pas$W0rd193")
-    print("version: 3.0")
+    print("version: 3.5")
     print("credits: created by orinion")
 
 def AboutMe():
     print("i am an AI prototype created by orinion.")
-    print("i am currently in version 3.0, but soon i will become an smart AI.")
+    print("i am currently in version 3.5")
     print("i am constantly learning and improving, so stay tuned for future updates!")
 
 while True:
@@ -39,47 +39,83 @@ while True:
             print()
             print("------------------------------------------------------------------------------------")
             print()
-            print("hello, " + name)
-            print("my name is AI.PROTOTYPE_385.")
-            print("(if you want, you can try a version that ChatGPT made)")
-            print()
-            print()
-            print("so what do you want to do " + name + "?")
-            print("the options are:")
-            print()
-            print("1. get to know more about me (write 'about me')")
-            print("2. exit the program (write 'exit')")
-            print("3. check the list (write 'list')")
-            print()
-            choise_from_options = input("please enter your choise: ")
-            if choise_from_options == "about me":
-                print("------------------------------------------------------------------------------------")
-                print()
-                AboutMe()
-                print()
-                print("------------------------------------------------------------------------------------")
-                print()
-            elif choise_from_options == "exit":
-                time.sleep(0.1)
-                print("exiting...")
-                time.sleep(1)
-                print("goodbye! see ya later!")
+            while True:
+                print("hello, " + name)
+                print("my name is AI.PROTOTYPE_385.")
                 print()
                 print()
+                print("so what do you want to do " + name + "?")
+                print("the options are:")
                 print()
+                print("1. get to know more about me (write 'about me')")
+                print("2. exit the program (write 'exit')")
+                print("3. check the list (write 'list')")
                 print()
-                print()
-                print("==test completed==")
+                choise_from_options = input("please enter your choise: ")
+                if choise_from_options == "about me":
+                    print("------------------------------------------------------------------------------------")
+                    print()
+                    AboutMe()
+                    print()
+                    print("------------------------------------------------------------------------------------")
+                    print()
+                    back_to_options=input("wanna go back to the options menu? (yes/no): ")
+                    if back_to_options == "yes" or back_to_options == "y" or back_to_options == "sure" or back_to_options == "yeah":
+                        time.sleep(0.1)
+                        print("going back to options menu...")
+                        time.sleep(1)
+                        continue
+                    else:
+                        time.sleep(0.1)
+                        print("exiting...")
+                        time.sleep(1)
+                        print("goodbye! see ya later!")
+                        print()
+                        print()
+                        print()
+                        print()
+                        print()
+                        print("==test completed==")
+                        exit()
 
-            elif choise_from_options == "list":
-                print("------------------------------------------------------------------------------------")
-                print()
-                List()
-                print()
-                print("------------------------------------------------------------------------------------")
-                print()
-                print("==test completed==")
+                elif choise_from_options == "exit":
+                    time.sleep(0.1)
+                    print("exiting...")
+                    time.sleep(1)
+                    print("goodbye! see ya later!")
+                    print()
+                    print()
+                    print()
+                    print()
+                    print()
+                    print("==test completed==")
+                    exit()
 
+                elif choise_from_options == "list":
+                    print("------------------------------------------------------------------------------------")
+                    print()
+                    List()
+                    print()
+                    print("------------------------------------------------------------------------------------")
+                    print()
+                    back_to_options=input("wanna go back to the options menu? (yes/no): ")
+                    if back_to_options == "yes" or back_to_options == "y" or back_to_options == "sure" or back_to_options == "yeah":
+                        time.sleep(0.1)
+                        print("going back to options menu...")
+                        time.sleep(1)
+                        continue
+                    else:
+                        time.sleep(0.1)
+                        print("exiting...")
+                        time.sleep(1)
+                        print("goodbye! see ya later!")
+                        print()
+                        print()
+                        print()
+                        print()
+                        print()
+                        print("==test completed==")
+                        exit()
         else:
             print("invalid choise. exiting...")
             time.sleep(1)
@@ -90,6 +126,7 @@ while True:
             print()
             print()
             print("==test completed==")
+            exit()
 
     else:
         time.sleep(0.1)
@@ -102,6 +139,6 @@ while True:
         print()
         print()
         print("==test completed==")
-    break
+        exit()
 
 # do not copy this code or other codes from the repository "python beginner codes". (in github)
